77604389645bcf827a0aaac6a4cf8f9ec643192c
IgorBarcelo <114026598+IgorBarcelo@users.noreply.github.com>
2025-05-13 18:39:09 -0300

    feat:(implantação frontend)

--- src/screens/info/index.js
+++ src/screens/info/index.js
@@ -2,108 +2,147 @@ import "./style.info.css"
 
 const Info = () => {
     return (
-         <div className="info-container">
-            <div className="info-header">
-                <h1 >COMO FUNCIONA</h1>
-                <span>
-                <p>Produzir uma peça customizada não é uma tarefa simples. É um trabalho que envolve muitas etapas e pessoas durante o processo. Buscamos simplificá-los ao máximo, para garantir a melhor produção!
-                </p>
-                <p>Veja quais informações você precisa saber para entrar em contato conosco e agilizar seu orçamento:</p>
-                </span>
+        <div className="info">
+            <div className="info-container">
+                <div className="info-header">
+                    <h1 >COMO FUNCIONA</h1>
+                    <span>
+                    <p>Produzir uma peça customizada não é uma tarefa simples. É um trabalho que envolve muitas etapas e pessoas durante o processo. Buscamos simplificá-los ao máximo, para garantir a melhor produção!
+                    </p>
+                    <p>Veja quais informações você precisa saber para entrar em contato conosco e agilizar seu orçamento:</p>
+                    </span>
+                </div>
+                <div className="steps-container">
+                    <div className="steps-bar"></div>
+                    <div className="steps">
+                    <div className="seta-direita">
+                        <svg preserveAspectRatio="none" data-bbox="19 39.5 162 121" viewBox="19 39.5 162 121" height="30" width="30" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true">
+                            <g>
+                                <path d="M59.5 79.833L19 120.167 59.5 160.5l40.5-40.333 40.5 40.333 40.5-40.333-40.5-40.334L100 39.5 59.5 79.833z" fill="#AB6F00"></path>
+                            </g>
+                        </svg>
+                    </div>
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="seta-direita2">
+                        <svg preserveAspectRatio="none" data-bbox="19 39.5 162 121" viewBox="19 39.5 162 121" height="30" width="30" xmlns="http://www.w3.org/2000/svg" data-type="shape" role="presentation" aria-hidden="true">
+                            <g>
+                                <path d="M59.5 79.833L19 120.167 59.5 160.5l40.5-40.333 40.5 40.333 40.5-40.333-40.5-40.334L100 39.5 59.5 79.833z" fill="#AB6F00"></path>
+                            </g>
+                        </svg>
+                    </div>
+                    <div className="steps-bar2"></div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="steps">
+                        <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
+                        <button>PRODUTO</button>
+                        <span>
+                            <a>Você já sabe qual produto irá desenvolver?</a>
+                        </span>
+                        <a></a>
+                        <span>
+                            <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
+                        </span>
+                    </div>
+                    <div className="pointer">
+                    <svg preserveAspectRatio="none" data-bbox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 200 200" role="presentation" aria-hidden="true">
+                        <g>
+                            <path d="M200 100c0 55.228-44.772 100-100 100S0 155.228 0 100 44.772 0 100 0s100 44.772 100 100z" fill="#AB6F00"></path>
+                        </g>
+                    </svg>
+                    </div>
+                </div>
             </div>
-            <div className="steps-container">
-                <div className="steps-bar"></div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps-bar2"></div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
-                </div>
-                <div className="steps">
-                    <img src="https://static.wixstatic.com/media/03db72_cee54bc70313499c8d05ecf3a5e4839c~mv2.png/v1/fill/w_200,h_200,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/Ícone%20Produtos.png" alt="produto"></img>
-                    <button>PRODUTO</button>
-                    <span>
-                        <a>Você já sabe qual produto irá desenvolver?</a>
-                    </span>
-                    <a></a>
-                    <span>
-                        <a>Veja as opções e suas variações de modelagem no link produtos e indique qual ou quais gostaria de produzir.</a>
-                    </span>
+            <div className="contact-container">
+                <div className="contact-bar">
+                    <div className="contact">
+                        <h1>ENTRE EM CONTATO</h1>
+                        <a>contato@malhariatropical.com.br</a>
+                        <svg></svg>
+                        <a>(11) 4933-1133</a>
+                        <svg></svg>
+                        <a>(11) 3331-1133</a>
+                    </div>
+                    <div className="budget">
+                        <h1>SOLICITE SEU ORÇAMENTO</h1>
+                        <button>CLIQUE AQUI PARA FAZER O SEU</button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </div>    
     )
 }
 
